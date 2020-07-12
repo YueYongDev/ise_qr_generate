c2d258778af9438fd2950badf4d42be126c06f74
lyy <yueyong1030@outlookcom>
2020-07-12 11:50:19 +0800

    update

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ class postMaker(object):
             pic_w, pic_h = postPic.size
 
             # 将封面图粘贴到背景图的指定位置，第二个参数为坐标
-            backImg.paste(postPic, ((bg_w-pic_w)//2, int((bg_h-pic_h)/4)))
+            backImg.paste(postPic, ((bg_w-pic_w)//2, int((bg_h-pic_h)/5.5)))
 
             draw = ImageDraw.Draw(backImg)
             draw.ink = textColor.get(
@@ -147,7 +147,7 @@ def get_title(url):
 
 
 if __name__ == "__main__":
-    url = 'https://www.toutiao.com/i6847837501932077572/'
+    url = 'https://www.toutiao.com/i6847670439016137229/'
     generateQRCode(url)
     # generatePost(get_title(url))
-    generatePost("软件文档问题揭露")
+    generatePost("深度代码搜索")
