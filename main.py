8c6aef537397fb45a635ff514dcca333eb981912
lyy <yueyong1030@outlookcom>
2020-09-08 10:35:03 +0800

    update

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ class postMaker(object):
             pic_w, pic_h = postPic.size
 
             # 将封面图粘贴到背景图的指定位置，第二个参数为坐标
-            backImg.paste(postPic, ((bg_w-pic_w)//2, int((bg_h-pic_h)/5.5)))
+            backImg.paste(postPic, ((bg_w-pic_w)//2, int((bg_h-pic_h)/4)))
 
             draw = ImageDraw.Draw(backImg)
             draw.ink = textColor.get(
@@ -147,7 +147,7 @@ def get_title(url):
 
 
 if __name__ == "__main__":
-    url = 'https://www.toutiao.com/i6869368561618387468/'
+    url = 'https://www.toutiao.com/i6869590981340037639/'
     generateQRCode(url)
     # generatePost(get_title(url))
-    generatePost("基于链接独立成分分析的多模态数据融合")
+    generatePost("基于类别深度典型相关分析的多模态数据下的细粒度场发现")
